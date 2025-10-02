859de9cc07eb9cf8d9a2589be30ec5ffe354c66f
Tobias Baumann <tobias.baumann@zib.de>
2025-10-02 19:24:39 +0200

    Remove Hello World text

--- FPGA/Intel/ADI2D_FP32/src/ADI2D_FP32_usm.cpp
+++ FPGA/Intel/ADI2D_FP32/src/ADI2D_FP32_usm.cpp
@@ -1,24 +1,10 @@
 //==============================================================
-// Vector Add is the equivalent of a Hello, World! sample for data parallel
-// programs. Building and running the sample verifies that your development
-// environment is setup correctly and demonstrates the use of the core features
-// of DPC++. This sample runs on both CPU and GPU (or FPGA). When run, it
-// computes on both the CPU and offload device, then compares results. If the
-// code executes on both CPU and offload device, the device name and a success
-// message are displayed. And, your development environment is setup correctly!
-//
-// For comprehensive instructions regarding DPC++ Programming, go to
-// https://software.intel.com/en-us/oneapi-programming-guide and search based on
-// relevant terms noted in the comments.
-//
-// DPC++ material used in the code sample:
-// •  A one dimensional array of data.
-// •  A device queue, buffer, accessor, and kernel.
-//==============================================================
 // Copyright © Intel Corporation
 //
 // SPDX-License-Identifier: MIT
 // =============================================================
+
+
 #include "dpc_common.hpp"
 #include <iostream>
 #include <sycl/sycl.hpp>
